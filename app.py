fb476c57f325ce27db9c54ffd544cd6ead2f7bd1
Chen, Hanlong <hchen778@gatech.edu>
2024-10-07 16:37:36 -0500

    adding all files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import os 
+
+
+app = Flask(__name__) # initializing a flask app
+
+@app.route('/',methods=['GET'])  # route to display the home page
+def homePage():
+    return render_template("index.html")
+
+
+@app.route('/train',methods=['GET'])  # route to train the pipeline
+def training():
+    return "Training Successful!" 
+
+
+
+
+if __name__ == "__main__":
+	# app.run(host="0.0.0.0", port = 8080, debug=True)
+	app.run(host="0.0.0.0", port = 8080)
